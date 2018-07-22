24eac492fd08a0cbea0d3b3fa73358b2d6c79cfa
Max <maxmal86@gmail.com>
2018-07-21 20:46:13 -0500

    Media query updates

--- resources/js/script.js
+++ resources/js/script.js
@@ -146,7 +146,7 @@ const init = async () => {
                 }
                 setTimeout(() => {
                     init();
-                }, 2000)
+                }, 1500)
             } else {
                 console.log(chosenCountry.flag);
                 console.log(clickedFlag2);
